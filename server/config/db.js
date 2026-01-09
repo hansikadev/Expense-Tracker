73fe01f446c00b4b773d61a884728506ee1c0d3e
hansikadev <tanwarhansika123@gmail.com>
2026-01-09 22:32:56 +0530

    build changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 const connectDB=async()=>{
     try{
-       const conn=await mongoose.connect(process.env.MONGODB_URI.replace('<PASSWORD>',process.env.DATABASEPASSWORD))
+       const conn=await mongoose.connect(process.env.MONGODB_URI)
        console.log("MongoDB connected:");
     }
     catch(error){
--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 const app=require('./app');
 const mongoose=require('mongoose');
 require('dotenv').config();
-const connectDB=require('./config/db');
+const connectDB = require('./config/db');
+const cors = require('cors');
 
 //database connection
 connectDB();
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
+  })
+);
 
-const port=process.env.FRONTEND_URL || 8000;
+const port=process.env.PORT || 8001;
 const server=app.listen(port,()=>{
     console.log('server is running on port',port);
 })
@@ -19,3 +26,5 @@ process.on("SIGINT",async()=>{
         process.exit(1);
     });
 })
+
+
